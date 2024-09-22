9b5390fe7a131523e7861b040a07d790b21f635d
DANPHILIP GLOVER-AKPEY <118954246+Jaydarex@users.noreply.github.com>
2024-09-22 15:05:21 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+st.title('MACHINE LEARNNG APP')
 df= pd.read_csv('https://raw.githubusercontent.com/Mawuyram/IDS/refs/heads/master/Final/IDS/lib/data/KDDTest%2B.arff.csv ')
 df
