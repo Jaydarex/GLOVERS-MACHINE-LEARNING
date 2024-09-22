58f3a5c0ac34860d771f420dedbd9c85a3c7cc20
DANPHILIP GLOVER-AKPEY <118954246+Jaydarex@users.noreply.github.com>
2024-09-22 15:23:43 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('MACHINE LEARNNG APP')
 
 with st.expander('DATA'):
   st.write('**RAW DATA**')
-df= pd.read_csv('https://raw.githubusercontent.com/Mawuyram/IDS/refs/heads/master/Final/IDS/lib/data/NSL-KDD.csv')
-df
+  df= pd.read_csv('https://raw.githubusercontent.com/Mawuyram/IDS/refs/heads/master/Final/IDS/lib/data/NSL-KDD.csv')
+  df
