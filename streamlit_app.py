bdfe3cc10c78fef1cca6811099c54826ae4c4354
DANPHILIP GLOVER-AKPEY <118954246+Jaydarex@users.noreply.github.com>
2024-09-22 15:02:47 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 App Name')
 
 st.write('Hello world!')
+df= pd.read_csv('https://raw.githubusercontent.com/Mawuyram/IDS/refs/heads/master/Final/IDS/lib/data/KDDTest%2B.arff.csv ')
+df
