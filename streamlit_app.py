cedd9cefa73038497177763827499dd2dc34f6be
DANPHILIP GLOVER-AKPEY <118954246+Jaydarex@users.noreply.github.com>
2024-09-22 15:22:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@ import streamlit as st
 import pandas as pd
 
 st.title('MACHINE LEARNNG APP')
+
+with st.expander('DATA'):
+  st.write('**RAW DATA**')
 df= pd.read_csv('https://raw.githubusercontent.com/Mawuyram/IDS/refs/heads/master/Final/IDS/lib/data/NSL-KDD.csv')
 df
